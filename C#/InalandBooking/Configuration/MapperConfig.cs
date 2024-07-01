3a22c16a32d15ecd8e8fc7f58366117196b7d951
Ina Zenelaj <149413552+zeneina@users.noreply.github.com>
2024-07-01 14:04:20 +0300

    Add files via upload

---
+++ C#/InalandBooking/Configuration/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using InalandBooking.Data;
+using InalandBooking.DTO;
+
+
+public class MapperConfig : Profile
+{
+    public MapperConfig()
+    {
+        CreateMap<User, UserPatchDTO>().ReverseMap();
+        CreateMap<User, UserSignupDTO>().ReverseMap();
+        CreateMap<User, UserDTO>().ReverseMap();
+    }
+}
